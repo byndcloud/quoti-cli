6e51b2315ab3c02b5880339e191f1d9983eb394c
Luiz Albuquerque <laaj@cin.ufpe.com.br>
2021-01-22 15:44:33 -0300

    inicializando repositorio

---
+++ index.js
@@ -0,0 +1,166 @@
+var firebase = require("firebase/app");
+const fs = require('fs');
+const readline = require("readline");
+
+
+require("firebase/auth");
+require("firebase/firestore");
+const md5 = require("md5");
+const axios = require("axios");
+const cliSelect = require('cli-select');
+const {Storage} = require('@google-cloud/storage');
+// Instantiate a storage client
+const storage = new Storage();
+const bucket = storage.bucket('dynamic-components');
+
+
+
+
+
+var institution = null
+var _token = null
+var extensionId = null
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDiicN8xT3lImJY0hfcobQfRLit90zMw8U",
+    authDomain: "beyond-quoti.firebaseapp.com",
+    databaseURL: "https://beyond-quoti.firebaseio.com",
+    projectId: "beyond-quoti",
+    storageBucket: "beyond-quoti.appspot.com",
+    messagingSenderId: "40570897776",
+    appId: "1:40570897776:web:4f02b3cf8eba78ed763bb5"
+};
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+var Args = process.argv.slice(2);
+console.log('Args: ', Args);
+
+async function insertIntitution(){
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+    return new Promise((resolve, reject) => {
+        rl.question('Qual sua instituição? ',  (answer) => {
+            rl.close();
+            resolve(answer);
+        });
+    });
+}
+async function insertToken(){
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+    return new Promise((resolve, reject) => {
+        rl.question('Informe o seu token de login ',  (answer) => {
+            rl.close();
+            resolve(answer);
+        });
+    });
+}
+
+async function login() {
+    institution = await insertIntitution()
+    let customToken = await insertToken()
+    const authFirebase = await app.auth().signInWithCustomToken(customToken)
+    const token = await firebase.auth().currentUser.getIdToken()
+    let data = JSON.stringify({institution:institution, user:authFirebase.user.toJSON()});
+    _token = token
+    fs.writeFileSync('credentials.json', data);
+}
+
+async function silentLogin(callsetup = false) {
+    let rawdata = null
+    let extensionValue = null
+    if (!fs.existsSync('credentials.json')) {
+        console.log('fazendo login')
+        await login()
+    }else{
+        try {
+            rawdata = fs.readFileSync('credentials.json');
+            const userData = (JSON.parse(rawdata)).user
+            extensionId = (JSON.parse(rawdata)).extensionId
+            extensionValue = (JSON.parse(rawdata)).extensionValue
+
+            const user = new firebase.User(userData, userData.stsTokenManager, userData)
+            await firebase.auth().updateCurrentUser(user)
+            const token = await firebase.auth().currentUser.getIdToken()
+            institution = (JSON.parse(rawdata)).institution
+            _token = token
+        } catch (error) {
+            console.log('erro ao carregar credenciais')
+        }
+    }
+    if(callsetup){
+        return extensionId
+    }
+    else if(!extensionId ){
+        console.log("Você já estar logado. Agora execute npm run setup para selecionar uma extensão")
+        process.exit(0)
+    }else{
+        console.log('Você está trabalhando na extensão ', extensionValue)
+    }
+    
+}
+async function getUploadFileName() {
+    return encodeURI(`${institution}/dev/idExtension${extensionId}.vue`)
+}
+async function sendExtensionsFile() {
+    console.log("Chamando a API")
+    await silentLogin()
+    // Create a new blob in the bucket and upload the file data.
+    // Uploads a local file to the bucket
+    let filename = await getUploadFileName()
+    await bucket.upload('./index.vue', {
+        destination: filename,
+        gzip: true,
+        metadata: {
+          cacheControl: 'public, max-age=0',
+        },  
+      });
+    const id = 'DdrMeO03Zd7tgDwFE0f7'
+    await firebase.firestore().collection('dynamicComponents').doc(id).update({
+        updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    console.log(`${filename} uploaded to ${'dynamic-components'}.`);
+    // [END storage_upload_file]
+}
+async function listExtensions() {
+    const result = await axios.get(`https://api.develop.minhaescola.app/api/v1/${institution}/dynamic-components/`,
+    {headers:{Authorization:`Bearer ${_token}`}})
+    // console.log(result.data)
+    return result.data
+}
+async function setup() {
+    await silentLogin('setup')
+    
+    
+    let extensions = await listExtensions()
+    rawdata = fs.readFileSync('credentials.json');
+    let credenciais = (JSON.parse(rawdata))
+    let mappedExt = extensions.map(el =>{
+        return el.title
+    })
+    let choose = await cliSelect({values:mappedExt})
+    credenciais.extensionId = extensions[choose.id].id
+    credenciais.extensionValue = choose.value
+    fs.writeFileSync('credentials.json', JSON.stringify(credenciais));
+    console.log("Agora execure npm run serve")
+
+}
+(function () {
+    if(Args[0] === "setup")
+        setup()
+    else if (Args[0] === "atualizar")
+        login()
+    else if (Args[0] === "login")
+        login()
+    else if (Args[0] === "silent-login")
+        silentLogin()
+    else 
+        sendExtensionsFile()
+        
+})()
---
+++ index.vue
@@ -0,0 +1,154 @@
+<template>
+  <v-tabs
+    v-model="tab"
+    align-with-title
+  >
+  
+    <v-tabs-slider color="blue"></v-tabs-slider>
+    <v-tab :key="'1'">
+      <span> Usando API Quoti </span>
+    </v-tab>
+    <v-tab :key="'2'">
+      <span> Usando o Firebase </span>
+    </v-tab>
+    <v-tabs-items v-model="tab">
+      <v-tab-item :key="'1'">
+        <v-row>
+          <v-col cols="12">
+            <v-data-table
+              :headers="headers"
+              :items="usersComputed"
+              :items-per-page="5"
+              class="elevation-1"
+            ></v-data-table>
+          </v-col>
+          <v-col cols="12">
+            <v-alert
+              color="green"
+              type="warning"
+            >Esse é um template exemplo de como podemos consultar
+             dados de todos os usuários cadastrados na Organização. Consulte o código para saber mais.</v-alert>
+          </v-col>
+
+        </v-row>
+      </v-tab-item>
+      <v-tab-item :key="'2'">
+        <v-col cols="12">
+          <v-alert
+            color="green"
+            type="warning"
+          >Esse é um template exemplo de como podemos consultar, adicionar, editar e deletar
+            dados do firebase. Consulte o código para saber mais.</v-alert>
+          <v-treeview
+            open-all
+            :items="itemsFirebase"
+          ></v-treeview>
+        </v-col>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-tabs>
+</template>
+<script>
+
+const Quoti = {
+  axios: config.axios,
+  moment: moment,
+  firestoreData: Firestore.collection(`/dynamicComponents/${id}/data/`).doc('collections'),
+  organizationApi: Organization,
+  userApi: User, 
+  presenceApi: Presence,
+  gradesApi: Grades,
+  catracaApi: Catraca,
+  postApi: Post,
+  notificationsApi: Notifications
+}
+
+export default {
+  props: {
+    date: String,
+    dateTitle: String,
+    group: Object,
+    person: Object,
+    reportType: String,
+    step: Number
+  },
+  data () {
+    return {
+      tab: null,
+      itemsFirebase: [
+        {
+          id: 15,
+          name: 'Downloads :',
+          children: [
+            { id: 16, name: 'October : pdf' },
+            { id: 17, name: 'November : pdf' },
+            { id: 18, name: 'Tutorial : html' },
+          ],
+        },
+        {
+          id: 19,
+          name: 'Videos :',
+          children: [
+            {
+              id: 20,
+              name: 'Tutorials :',
+              children: [
+                { id: 21, name: 'Basic layouts : mp4' },
+                { id: 22, name: 'Advanced techniques : mp4' },
+                { id: 23, name: 'All about app : dir' },
+              ],
+            },
+            { id: 24, name: 'Intro : mov' },
+            { id: 25, name: 'Conference introduction : avi' },
+          ],
+        },
+      ],
+      headers: [
+        { text: 'Usuário', align: 'start', value: 'name' },
+        { text: 'CPF', value: 'cpf' },
+        { text: 'Type', value: 'type' }
+      ],
+      users: ''    
+    }
+  },
+  computed: {
+    usersComputed: function () {
+      return this?.users ? this.users : []
+    } 
+  },
+  async created() {
+    console.log('The this is: ', this)
+    console.log('The Quoti axios: ', Quoti.axios)
+    console.log('The Quoti moment: ', Quoti.moment)
+    //usando userApi
+    const result = await Quoti.userApi.list({
+      types: undefined
+    })
+    console.log(this.users)
+    this.users = result
+    console.log(result)
+    console.log(this.users)
+
+    // usando o firebase
+    Quoti.firestoreData.collection("users").add({
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    }).then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    
+      Quoti.firestoreData.getCollections().then(function(doc) {
+          if (doc.exists) {
+              console.log("Document data:", doc.data());
+          } else {
+              console.log("No such document!");
+          }
+      }).catch(function(error) {
+          console.log("Error getting document:", error);
+      });
+    })
+  }
+}
+</script>
+
+
