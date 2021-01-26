89704d5a95809650bcc216d7c45abc303cb0fbab
Victor Aurélio <ovictoraurelio@gmail.com>
2021-01-26 16:55:51 -0300

    feat: add eslint and jsconfig to project

--- cli/options/login.js
+++ cli/options/login.js
@@ -1,36 +1,37 @@
-const {  app } = require('./firebase')
+const { app } = require('./firebase')
+const readline = require('readline')
+const fs = require('fs')
 
-async function insertIntitution() {
+async function insertIntitution () {
   const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-  });
+    input: process.stdin,
+    output: process.stdout
+  })
   return new Promise((resolve, reject) => {
-      rl.question('Qual sua instituição? ', (answer) => {
-          rl.close();
-          resolve(answer);
-      });
-  });
+    rl.question('Qual sua instituição? ', answer => {
+      rl.close()
+      resolve(answer)
+    })
+  })
 }
 
-async function insertToken() {
+async function insertToken () {
   const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-  });
+    input: process.stdin,
+    output: process.stdout
+  })
   return new Promise((resolve, reject) => {
-      rl.question('Informe o seu token de login ', (answer) => {
-          rl.close();
-          resolve(answer);
-      });
-  });
+    rl.question('Informe o seu token de login ', answer => {
+      rl.close()
+      resolve(answer)
+    })
+  })
 }
-
 
 module.exports = async function () {
-  institution = await insertIntitution()
+  const institution = await insertIntitution()
   let customToken = await insertToken()
   const authFirebase = await app.auth().signInWithCustomToken(customToken)
-  let data = JSON.stringify({ institution: institution, user: authFirebase.user.toJSON() });
-  fs.writeFileSync('credentials.json', data);
+  let data = JSON.stringify({ institution: institution, user: authFirebase.user.toJSON() })
+  fs.writeFileSync('credentials.json', data)
 }
