0f16d2841ee3c1be235ebcd1bca54a016a804d0f
Luiz Albuquerque <laaj@cin.ufpe.com.br>
2021-01-29 14:44:16 -0300

    style(Axios config): remove logs

--- src/config/axios.js
+++ src/config/axios.js
@@ -8,7 +8,6 @@ class Api {
     this.create()
   }
   async create () {
-    console.log(process.env.API_BASE_URL)
     this.axios = axios.create({
       baseURL: process.env.API_BASE_URL || `https://api.develop.minhaescola.app/api/v1/`
     //   baseURL: `https://api.develop.minhaescola.app/api/v1/`
