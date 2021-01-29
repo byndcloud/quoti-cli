864122d49977a5b4ac11fc9fb6c46ed3f9b71d0f
Luiz Albuquerque <laaj@cin.ufpe.com.br>
2021-01-29 14:48:53 -0300

    style(login): remove logs

--- src/hooks/login.js
+++ src/hooks/login.js
@@ -1,7 +1,5 @@
 const Auth = require('../services/auth')
 
 module.exports = async function (options) {
-  console.log(`Login hook started`)
   await Auth.silentLogin()
-  console.log(`Login hook finished`)
 }
