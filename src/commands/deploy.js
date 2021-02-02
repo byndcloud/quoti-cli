2e5c524ae87e055ad3a2e7ca3a293b4114be58f5
Luiz Albuquerque <laaj@cin.ufpe.com.br>
2021-02-02 11:18:51 -0300

    style(deploy): improve message text (interface)

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -6,53 +6,70 @@ const credentials = require('../config/credentials')
 const { default: Command } = require('@oclif/command')
 const chalk = require('chalk')
 const api = require('../config/axios')
+const readline = require('readline')
 
 class DeployCommand extends Command {
   async run () {
     await credentials.load()
-    const currentTime = await firebase.firestore.Timestamp.fromDate(new Date()).toMillis()
-    const filename = this.getUploadFileNameDeploy(currentTime.toString())
-    const url = `https://storage.cloud.google.com/dynamic-components/${filename}`
+    try {
+      const currentTime = await firebase.firestore.Timestamp.fromDate(new Date()).toMillis()
+      const versionName = await this.inputVersionName() || currentTime
+      const filename = this.getUploadFileNameDeploy(currentTime.toString())
+      const url = `https://storage.cloud.google.com/dynamic-components/${filename}`
 
-    const { args } = this.parse(DeployCommand)
+      const { args } = this.parse(DeployCommand)
 
-    if (!credentials.extensionId) {
-      console.log(chalk.yellow('Please select your extension. Try run qt selectExtension'))
-      process.exit(0)
-    } else if (!fs.existsSync(args.filePath)) {
-      console.log(chalk.red(`File ${args.filePath} not found`))
-      process.exit(0)
-    }
-
-    await bucket.upload(args.filePath, {
-      destination: filename,
-      gzip: true,
-      metadata: {
-        cacheControl: 'public, max-age=0'
+      if (!credentials.extensionId) {
+        console.log(chalk.yellow('Please select your extension. Try run qt selectExtension'))
+        process.exit(0)
+      } else if (!fs.existsSync(args.filePath)) {
+        console.log(chalk.red(`File ${args.filePath} not found`))
+        process.exit(0)
       }
-    })
-    const token = await firebase.auth().currentUser.getIdToken()
-    const result = await api.axios.put(
-      `/${credentials.institution}/dynamic-components/${credentials.extensionId}`,
-      {
-        url: url,
-        version: currentTime,
-        fileVuePrefix: filename,
-        id: credentials.extensionId
-      },
-      { headers: { Authorization: `Bearer ${token}` } }
-    )
-    await firebase
-      .firestore()
-      .collection('dynamicComponents')
-      .doc(credentials.extensionStorageId)
-      .update({
-        updatedAtToDeploy: currentTime
-      })
-    console.log(chalk.green('Deploy done!'))
-    process.exit(0)
-  }
 
+      await bucket.upload(args.filePath, {
+        destination: filename,
+        gzip: true,
+        metadata: {
+          cacheControl: 'public, max-age=0'
+        }
+      })
+      const token = await firebase.auth().currentUser.getIdToken()
+      await api.axios.put(
+        `/${credentials.institution}/dynamic-components/${credentials.extensionId}`,
+        {
+          url: url,
+          version: versionName,
+          fileVuePrefix: filename,
+          id: credentials.extensionId
+        },
+        { headers: { Authorization: `Bearer ${token}` } }
+      )
+      await firebase
+        .firestore()
+        .collection('dynamicComponents')
+        .doc(credentials.extensionStorageId)
+        .update({
+          updatedAtToDeploy: currentTime
+        })
+      console.log(chalk.green('Deploy done!'))
+      process.exit(0)
+    } catch (error) {
+      console.log(chalk.red(error))
+    }
+  }
+  async inputVersionName () {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+    return new Promise((resolve, reject) => {
+      rl.question(`Version Name: `, answer => {
+        rl.close()
+        resolve(answer)
+      })
+    })
+  }
   getUploadFileNameDeploy (currentTime) {
     return encodeURI(`${credentials.institution}/${md5(currentTime)}.vue`)
   }
