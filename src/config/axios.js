5f7472ff86d601b607e5f2e4254db48d1bbcbe88
Luiz Albuquerque <laaj@cin.ufpe.com.br>
2021-02-15 13:46:44 -0300

    fix: changing address request at axios config

--- src/config/axios.js
+++ src/config/axios.js
@@ -8,9 +8,12 @@ class Api {
     this.create()
   }
   async create () {
+    console.log(process.env.API_BASE_URL)
     this.axios = axios.create({
-      baseURL: process.env.API_BASE_URL || `https://api.develop.minhaescola.app/api/v1/`
+      baseURL: process.env.API_BASE_URL || `https://api.minhafaculdade.app/api/v1/`
+      // baseURL: process.env.API_BASE_URL || `https://api.develop.minhaescola.app/api/v1/`
     //   baseURL: `https://api.develop.minhaescola.app/api/v1/`
+    // https://api.minhafaculdade.app/api/v1/
     })
   }
 }
