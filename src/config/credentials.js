49d62dcf76fbfebd61c3f4f2ead0e72fa7c7bd6f
Nathan <nmf2@cin.ufpe.br>
2021-03-03 14:13:30 -0300

    fix: save credentials in users' home directory

--- src/config/credentials.js
+++ src/config/credentials.js
@@ -1,14 +1,20 @@
 const chalk = require('chalk')
 const JSONManager = require('./JSONManager')
 const fs = require('fs')
+const path = require('path')
+
+const home = require('os').homedir()
+const baseConfigDirectory = path.join(home, '.config/quoti-cli/')
 
 try {
-  fs.mkdirSync('~/.config/quoti-cli/', { recursive: true })
+  fs.mkdirSync(baseConfigDirectory, { recursive: true })
 } catch (e) {
   console.error(
-    chalk.red("Couldn't create folder at ~/.config/quoti-cli/, message:")
+    chalk.red(`Couldn't create folder at ${baseConfigDirectory}, message:`)
   )
   console.error(chalk.red(e))
 }
 
-module.exports = new JSONManager('~/.config/quoti-cli/credentials.json')
+module.exports = new JSONManager(
+  path.join(baseConfigDirectory, 'credentials.json')
+)
