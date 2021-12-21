91550ad903154c1a1aa44ed5880fadc722fb636d
Nathan Freire <nmf2@cin.ufpe.br>
2021-12-21 17:28:42 -0300

    feat(select-extension): add extension to package.json and improve DX with better error messages

--- src/commands/select-extension.js
+++ src/commands/select-extension.js
@@ -1,57 +1,204 @@
-const { app } = require('../config/firebase')
-const cliSelect = require('cli-select')
-const credentials = require('../config/credentials')
-const { default: Command } = require('@oclif/command')
+const path = require('path')
+const ora = require('ora')
 const chalk = require('chalk')
+const { union } = require('lodash')
+const inquirer = require('inquirer')
+const readJSON = require('json-file-plus')
+const readPkgSync = require('read-pkg-up').sync
+
+const { merge } = require('lodash')
+const { readdirSync, existsSync } = require('fs')
+const { default: Command, flags } = require('@oclif/command')
+
+const { app } = require('../config/firebase')
+
+const credentials = require('../config/credentials')
 const api = require('../config/axios')
 const JSONManager = require('../config/JSONManager')
 
 class SelectExtensionCommand extends Command {
   constructor () {
     super(...arguments)
-    this.manifest = new JSONManager('./manifest.json')
-  }
-  async run () {
-    try {
-      const extensions = await this.listExtensions(credentials.institution)
-      const mappedExt = extensions.map(el => el.title)
-      console.log(chalk.bgMagentaBright('Choose your extension to work'))
-      const choose = await cliSelect({
-        unselected: '○',
-        selected: '●',
-        values: mappedExt,
-        valueRenderer: (value, selected) => {
-          if (selected) {
-            return chalk.bgMagenta(value)
-          }
-
-          return value
-        }
-      }) // TODO: Replace cliSelect with an oclif plugin
-      this.manifest.extensionId = extensions[ choose.id ].id
-      this.manifest.extensionStorageId = extensions[ choose.id ].storeId
-      this.manifest.type = extensions[ choose.id ].type === 'Com build' ? 'build' : 'noBuild'
-      this.manifest.name = choose.value
-      this.manifest.save()
-      console.log(chalk.green(`Your extension is ${choose.value}`))
-      console.log(chalk.yellow('Now run qt serve'))
-    } catch (error) {
-      console.log(chalk.red(error))
+    const pkgInfo = readPkgSync()
+    if (pkgInfo) {
+      this.projectRoot = path.resolve(path.dirname(pkgInfo.path))
+      this.packageJsonPath = pkgInfo.path
+    } else {
+      this.projectRoot = process.cwd()
     }
   }
-  async listExtensions (institution) {
-    const token = await app.auth().currentUser.getIdToken()
-    const result = await api.axios.get(
-      `/${institution}/dynamic-components/`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
+
+  async run () {
+    try {
+      const { args, flags } = this.parse(SelectExtensionCommand)
+
+      if (args.entryPointPath && !existsSync(args.entryPointPath)) {
+        throw new Error(
+          `The given extension entrypoint file wasn't found at '${args.entryPointPath}', make sure the file exists`
+        )
       }
+
+      if (args.entryPointPath && !args.entryPointPath.endsWith('.vue')) {
+        throw new Error(`The extension entrypoint file must be a .vue file`)
+      }
+
+      if (!this.packageJsonPath && flags.build === true) {
+        throw new Error(
+          'To select an extension with build you have to be in a Vue project with a package.json file'
+        )
+      }
+
+      const spinner = ora({
+        text: 'Fetching extensions',
+        spinner: 'dots3'
+      }).start()
+
+      const extensions = await this.listExtensions(
+        credentials.institution,
+        flags.build
+      ).catch(err => {
+        spinner.fail('Failed loading extensions')
+        throw err
+      })
+
+      if (extensions.length === 0) {
+        spinner.fail("Couldn't find any extensions")
+        return
+      }
+
+      spinner.succeed('Got extensions list!')
+      const choices = extensions.map(ext => ({ name: ext.title, value: ext }))
+      const { selectedExtension, selectedEntryPoint } = await inquirer.prompt([
+        {
+          name: 'selectedExtension',
+          message: 'Choose your extension:',
+          type: 'list',
+          choices
+        },
+        {
+          name: 'selectedEntryPoint',
+          message: 'Which file is the entry point (main) to your extension?',
+          type: 'list',
+          choices: this.getFilesAsChoices(),
+          when: !args.entryPointPath
+        }
+      ])
+
+      const absoluteExtensionPath = path.resolve(
+        args.entryPointPath || selectedEntryPoint
+      )
+
+      if (this.packageJsonPath) {
+        await this.addExtensionToPackageJson(absoluteExtensionPath)
+      }
+
+      this.upsertManifest(absoluteExtensionPath, selectedExtension)
+
+      console.log(chalk.green('Extension selected! \\o/'))
+      console.log(
+        chalk.blue("You can now go to your project's root and run"),
+        chalk.yellow('qt serve')
+      )
+    } catch (error) {
+      console.log(chalk.red(error))
+      if (process.env.DEBUG) console.error(error)
+    }
+  }
+
+  upsertManifest (extensionPath, extensionData) {
+    const manifest = new JSONManager(
+      path.resolve(path.dirname(extensionPath), 'manifest.json')
     )
-    return result.data
+
+    manifest.extensionId = extensionData.id
+    manifest.extensionStorageId = extensionData
+    manifest.type = extensionData.type === 'Com build' ? 'build' : 'noBuild'
+    manifest.name = extensionData.title
+
+    manifest.save()
+
+    return manifest
+  }
+
+  async addExtensionToPackageJson (absoluteExtensionPath) {
+    const extensionPathRelativeToProjectRoot = path.relative(
+      this.projectRoot,
+      absoluteExtensionPath
+    )
+
+    const packageJsonEditor = await readJSON(path.resolve(this.packageJsonPath))
+
+    const currentQuotiInfo = merge(
+      { extensions: [] },
+      await packageJsonEditor.get('quoti')
+    )
+
+    currentQuotiInfo.extensions = union(currentQuotiInfo.extensions, [
+      extensionPathRelativeToProjectRoot
+    ])
+
+    packageJsonEditor.set({
+      quoti: currentQuotiInfo
+    })
+
+    await packageJsonEditor.save()
+  }
+
+  async listExtensions (institution, withBuild) {
+    const token = await app.auth().currentUser.getIdToken()
+    const result = await api.axios.get(`/${institution}/dynamic-components/`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    let extensions
+
+    if (typeof withBuild !== 'undefined') {
+      extensions = result.data.filter(
+        extension =>
+          (withBuild && extension.type === 'Com build') ||
+          (!withBuild && extension.type === 'Sem build')
+      )
+    } else {
+      extensions = result.data
+    }
+
+    return extensions
+  }
+
+  getFilesAsChoices (fileExtensionsAllowed = ['.vue']) {
+    const dirents = readdirSync(process.cwd(), { withFileTypes: true })
+
+    const filesNames = dirents
+      .filter(dirent => dirent.isFile())
+      .filter(dirent =>
+        fileExtensionsAllowed.some(fileExt => dirent.name.endsWith(fileExt))
+      )
+      .map(dirent => dirent.name)
+
+    return filesNames.map(fileName => ({ name: fileName, value: fileName }))
   }
 }
+
+SelectExtensionCommand.args = [
+  {
+    name: 'entryPointPath',
+    required: false,
+    description: "The path to the Extension's entry point"
+  }
+]
+
+SelectExtensionCommand.flags = {
+  build: flags.boolean({
+    allowNo: true,
+    char: 'b',
+    description:
+      "Specify that you're selecting an extension **with** build, use --no-build for extensions without build",
+    exclusive: []
+  })
+}
+
 SelectExtensionCommand.description = `Select your extension to work`
-// TODO: Find a way to customize command name
+
 module.exports = SelectExtensionCommand
