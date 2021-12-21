8c92e0ff5687b9e577647a1493179579a28d21a2
Nathan Freire <nmf2@cin.ufpe.br>
2021-12-20 22:34:56 -0300

    feat(serve): rebuild only extensions that changed

--- src/commands/serve.js
+++ src/commands/serve.js
@@ -1,57 +1,167 @@
-const credentials = require('../config/credentials')
 const fs = require('fs')
-const { default: Command } = require('@oclif/command')
+const Ws = require('ws')
+const path = require('path')
 const chalk = require('chalk')
 const chokidar = require('chokidar')
-const ExtensionService = require('../services/extension')
+const readPkgSync = require('read-pkg-up').sync
+const getDependencyTree = require('get-dependency-tree')
+
 const { debounce } = require('lodash')
-const path = require('path')
+const { default: Command } = require('@oclif/command')
+
+const credentials = require('../config/credentials')
+const ExtensionService = require('../services/extension')
 const JSONManager = require('../config/JSONManager')
 
-class ServeCommand extends Command {
-  buildAndUpload (args) {
-    if (!this.manifest) {
-      const manifestPath = path.resolve(path.dirname(args.filePath), 'manifest.json')
-      this.manifest = new JSONManager(manifestPath)
-    }
-    return async (event, path) => {
-      let distPath = args.filePath
+const connectionUrl =
+  'wss://connect.websocket.in/v3/1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm&notify_self'
 
-      if (this.manifest.type === 'build') {
-        distPath = `./dist/dc_${this.manifest.extensionId}.umd.min.js`
-        console.log(`Building extension...`)
-        await this.extensionService.build(args.filePath, { mode: 'staging' })
-      }
-      console.log(`Uploading file ${distPath}...`)
-      await this.extensionService.upload(fs.readFileSync(distPath), this.getUploadFileName())
+const options = {
+  headers: {
+    Authorization:
+      'Binary 01100100 01100001 01110110 01101001 01100100 01100101 01110110',
+    Cookie: 'uuid=dev-server-whiskas-sachê'
+  }
+}
+const socket = new Ws(connectionUrl, options)
+
+class ServeCommand extends Command {
+  constructor () {
+    super(...arguments)
+
+    credentials.load()
+
+    const pkgInfo = readPkgSync()
+    if (!pkgInfo?.packageJson) {
+      throw new Error(
+        'Nenhum arquivo package.json encontrado, tem certeza que o diretório atual é de um projeto Vue?'
+      )
+    }
+
+    this.projectRoot = path.resolve(path.dirname(pkgInfo.path))
+    this.extensionsPaths = pkgInfo.packageJson.quoti.extensions.map(extPath =>
+      path.resolve(this.projectRoot, extPath)
+    )
+    if (this.extensionsPaths.length === 0) {
+      throw new Error(
+        'Nenhuma extensão declarada no package.json, adicione o entrypoint da sua extensão em um array no path quoti.extensions dentro do package.json'
+      )
     }
   }
+  buildAndUpload (args) {
+    return async changedFilePath => {
+      if (!changedFilePath) return
+
+      const extensionsEntrypointsToCheck = []
+      const currentExtensionPath = args.filePath
+        ? path.resolve(args.filePath)
+        : null
+
+      if (
+        currentExtensionPath &&
+        !this.extensionsPaths.includes(currentExtensionPath)
+      ) {
+        throw new Error(
+          `O caminho especificado (${args.filePath}) não foi declarado como de uma extensão no package.json em quoti.extensions.`
+        )
+      }
+
+      if (!currentExtensionPath) {
+        extensionsEntrypointsToCheck.push(...this.extensionsPaths)
+      } else {
+        extensionsEntrypointsToCheck.push(currentExtensionPath)
+      }
+
+      const changedFileAbsolutePath = path.resolve(changedFilePath)
+
+      const extensionsToUpdate = extensionsEntrypointsToCheck.filter(
+        entryPoint => {
+          const { arr: dependencies } = getDependencyTree({ entry: entryPoint })
+          // console.log(`entry`, entryPoint, dependencies)
+          return dependencies.includes(changedFileAbsolutePath)
+        }
+      )
+
+      const manifests = extensionsToUpdate.reduce(
+        (manifestsObj, entryPoint) => {
+          manifestsObj[entryPoint] = this.getManifestFromEntryPoint(entryPoint)
+          return manifestsObj
+        },
+        {}
+      )
+
+      Object.entries(manifests).forEach(([path, manifest]) => {
+        if (!manifest?.exists()) {
+          throw new Error(
+            `manifest.json não encontrado para a extensão em ${path}`
+          )
+        }
+      })
+
+      const extensionsData = await Promise.all(
+        extensionsToUpdate.map(async entryPoint => {
+          let distPath = null
+          const manifest = manifests[entryPoint]
+          const extensionService = new ExtensionService(manifest)
+          if (manifest.type === 'build') {
+            distPath = `./dist/dc_${manifest.extensionId}.umd.min.js`
+            console.log(`Building extension...`)
+            await extensionService.build(entryPoint, { mode: 'staging' })
+            console.log(
+              `Built extension ${entryPoint.replace(this.projectRoot, '')}`
+            )
+          }
+          console.log(`Uploading file ${distPath}...`)
+          const extensionCode = fs
+            .readFileSync(distPath || changedFilePath)
+            .toString()
+
+          // extensionService.upload(fileBuffer, this.getUploadFileName())
+          return {
+            extensionInfo: manifests[entryPoint],
+            code: extensionCode
+          }
+        })
+      )
+
+      extensionsData.forEach(extensionData => {
+        const message = `Built and uploaded extension ${extensionData.extensionInfo.name}`
+        socket.send(
+          JSON.stringify({
+            event: 'reload-extension',
+            data: {
+              ...extensionData,
+              user: {
+                uid: credentials.user.uid,
+                orgSlug: credentials.institution
+              }
+            }
+          })
+        )
+        console.log(message)
+      })
+    }
+  }
+
+  getManifestFromEntryPoint (changedFilePath) {
+    const manifestPath = path.resolve(
+      path.dirname(changedFilePath),
+      'manifest.json'
+    )
+    const manifest = new JSONManager(manifestPath)
+    return manifest
+  }
+
   async run () {
-    await credentials.load()
     try {
       const { args } = this.parse(ServeCommand)
-      const manifestPath = path.resolve(path.dirname(args.filePath), 'manifest.json')
-      this.manifest = new JSONManager(manifestPath)
-      this.extensionService = new ExtensionService(this.manifest)
-
-      console.log(`Changes saved in ${args.filePath} will be displayed on the develop page`)
-      console.log(chalk.green(`Waiting for changes ...`))
-
-      if (!this.manifest.exists()) {
-        console.log(chalk.yellow('Please select your extension. Try run qt selectExtension'))
-        process.exit(0)
-      }
-      if (!fs.existsSync(args.filePath) || args.filePath.slice(-4) !== '.vue') {
-        console.log(chalk.red(`Path ${args.filePath} is not valid directory`))
-        process.exit(0)
-      }
-      await this.manifest.load()
-
-      const filesToWatch = [ args.filePath, '*.js', './**/*.vue', './**/*.js' ]
+      const filesToWatch = ['*.js', './**/*.vue', './**/*.js']
+      if (args.filePath) filesToWatch.push(args.filePath)
 
       const debouncedBuild = debounce(this.buildAndUpload(args), 800)
-      chokidar.watch(filesToWatch, { ignored: [ 'node_modules' ] }).on('change', debouncedBuild)
-      chokidar.watch(filesToWatch, { ignored: [ 'node_modules' ] }).on('ready', debouncedBuild)
+      chokidar
+        .watch(filesToWatch, { ignored: ['node_modules'] })
+        .on('change', debouncedBuild)
     } catch (error) {
       console.log(chalk.red(`${error}`))
     }
@@ -63,9 +173,7 @@ class ServeCommand extends Command {
     } else {
       path += '.vue'
     }
-    return encodeURI(
-      path
-    )
+    return encodeURI(path)
   }
 }
 
@@ -73,8 +181,7 @@ ServeCommand.args = [
   {
     name: 'filePath',
     required: false,
-    description: 'The path to a file to build',
-    default: './src/App.vue'
+    description: 'The path to a file to build'
   }
 ]
 ServeCommand.description = `Create local serve and Upload file automatically
