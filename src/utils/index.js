3a47aa3660b84f98160770d0b490ddd322e189c9
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-01-21 12:28:28 -0300

    feat: add getManifestFromEntryPoint function to index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,16 @@
+const JSONManager = require('../config/JSONManager')
+const path = require('path')
 class Utils {
   isYes (text) {
     return ['s', 'sim', 'yes', 'y'].includes(text.toLowerCase())
+  }
+  getManifestFromEntryPoint (entrypointPath) {
+    const manifestPath = path.resolve(
+      path.dirname(entrypointPath),
+      'manifest.json'
+    )
+    const manifest = new JSONManager(manifestPath)
+    return manifest
   }
 }
 
