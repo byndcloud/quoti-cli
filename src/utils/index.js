e3777897d4d4e498c129f015f69b7b502dee9f12
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-01-27 08:33:01 -0300

    feat: add confirmQuestion function on utils/index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,29 @@
 const JSONManager = require('../config/JSONManager')
 const path = require('path')
+const inquirer = require('inquirer')
 class Utils {
   isYes (text) {
     return ['s', 'sim', 'yes', 'y'].includes(text.toLowerCase())
+  }
+  isNo (text) {
+    return ['n', 'não', 'nao', 'no'].includes(text.toLowerCase())
+  }
+  async confirmQuestion (text) {
+    const { confirmVersion } = await inquirer.prompt([
+      {
+        name: 'versionName',
+        message: text,
+        type: 'input',
+        validate: input => {
+          if (!this.isYes(input) && !this.isNo(input)) {
+            return 'Só é permitido "Sim" ou "Não" como resposta'
+          }
+          return true
+        }
+
+      }
+    ])
+    return confirmVersion
   }
   getManifestFromEntryPoint (entrypointPath) {
     const manifestPath = path.resolve(
