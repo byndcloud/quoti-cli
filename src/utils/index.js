5c3a6795c796dfb4a10ea368f573c2d1ad4589e6
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-02-03 14:26:52 -0300

    refactor: utils exports functions instead of instance of class

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,54 +8,60 @@ function isYes (text) {
 function isNo (text) {
   return ['n', 'não', 'nao', 'no'].includes(text.toLowerCase())
 }
-class Utils {
-  async confirmQuestion (text) {
-    const { confirmVersion } = await inquirer.prompt([
-      {
-        name: 'confirmVersion',
-        message: text,
-        type: 'input',
-        validate: input => {
-          if (!isYes(input) && !isNo(input)) {
-            return 'Só é permitido "Sim" ou "Não" como resposta'
-          }
-          return true
+
+async function confirmQuestion (text) {
+  const { confirmVersion } = await inquirer.prompt([
+    {
+      name: 'confirmVersion',
+      message: text,
+      type: 'input',
+      validate: input => {
+        if (!isYes(input) && !isNo(input)) {
+          return 'Só é permitido "Sim" ou "Não" como resposta'
         }
-
+        return true
       }
-    ])
-    return isYes(confirmVersion)
-  }
-  getManifestFromEntryPoint (entrypointPath) {
-    const manifestPath = path.resolve(
-      path.dirname(entrypointPath),
-      'manifest.json'
-    )
-    const manifest = new JSONManager(manifestPath)
-    return manifest
-  }
-  getProjectRootPath () {
-    const pkgInfo = readPkgSync()
-    if (!pkgInfo) {
-      throw new Error(
-        'Para executar determinado comando você precisa estar em um projeto Vue com um arquivo package.json. Execute npm init na raiz do projeto ou use um modelo.'
-      )
-    }
 
-    return path.resolve(path.dirname(pkgInfo.path))
-  }
-  listExtensionsPaths () {
-    const pkgInfo = readPkgSync()
-    if (!pkgInfo?.packageJson) {
-      throw new Error(
-        'Nenhum arquivo package.json encontrado, tem certeza que o diretório atual é de um projeto Vue?'
-      )
     }
-    const projectRoot = path.resolve(path.dirname(pkgInfo.path))
-    return pkgInfo.packageJson.quoti.extensions.map(extPath =>
-      path.resolve(projectRoot, extPath)
+  ])
+  return isYes(confirmVersion)
+}
+function getManifestFromEntryPoint (entrypointPath) {
+  const manifestPath = path.resolve(
+    path.dirname(entrypointPath),
+    'manifest.json'
+  )
+  const manifest = new JSONManager(manifestPath)
+  return manifest
+}
+function getProjectRootPath () {
+  const pkgInfo = readPkgSync()
+  if (!pkgInfo) {
+    throw new Error(
+      'Para executar determinado comando você precisa estar em um projeto Vue com um arquivo package.json. Execute npm init na raiz do projeto ou use um modelo.'
     )
   }
+
+  return path.resolve(path.dirname(pkgInfo.path))
+}
+function listExtensionsPaths () {
+  const pkgInfo = readPkgSync()
+  if (!pkgInfo?.packageJson) {
+    throw new Error(
+      'Nenhum arquivo package.json encontrado, tem certeza que o diretório atual é de um projeto Vue?'
+    )
+  }
+  const projectRoot = getProjectRootPath()
+  return pkgInfo.packageJson.quoti.extensions.map(extPath =>
+    path.resolve(projectRoot, extPath)
+  )
 }
 
-module.exports = new Utils()
+module.exports = {
+  isYes,
+  isNo,
+  confirmQuestion,
+  getManifestFromEntryPoint,
+  getProjectRootPath,
+  listExtensionsPaths
+}
