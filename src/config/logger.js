bb45c2102e29c6463a66040fa2dc2f9c225ab500
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-02-07 11:48:26 -0300

    feat: logger print message according enverionment variable

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,6 +1,9 @@
 const { createLogger, format, transports, addColors } = require('winston')
 const { combine, timestamp, printf } = format
+const dotenv = require('dotenv')
+dotenv.config()
 
+const NODE_ENV = process.env.NODE_ENV
 const colorizer = format.colorize()
 
 const myCustomLevels = {
@@ -25,15 +28,16 @@ const logger = createLogger({
   format: combine(
     timestamp(),
     printf(msg => {
-      if (msg.level === 'debug') {
+      if (msg.level === 'debug' && NODE_ENV === 'development') {
         if (msg.tag) {
           return colorizer.colorize(msg.level, `${msg.timestamp} - ${msg.tag} - ${msg.level}: ${msg.message}`)
         } else {
           return colorizer.colorize(msg.level, `${msg.timestamp} - ${msg.level}: ${msg.message}`)
         }
-      } else {
-        return colorizer.colorize(msg.level, `${msg.message}`)
+      } else if (msg.stack && NODE_ENV === 'development') {
+        return colorizer.colorize(msg.level, `${msg.stack}`)
       }
+      return colorizer.colorize(msg.level, `${msg.message}`)
     })
   ),
   transports: [
