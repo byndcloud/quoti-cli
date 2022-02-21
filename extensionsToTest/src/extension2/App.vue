3faa5247d9215f69e58e69f76eb4f8825ae1bdad
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-02-21 14:53:06 -0300

    feat: add extensionsToTest

---
+++ extensionsToTest/src/extension2/App.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+    v-col
+      p Extensão 1(com build)
+      MyComponent
+</template>
+
+<script>
+import MyComponent from './MyComponent'
+export default {
+  name: 'extension 2',
+  components: {
+    MyComponent
+  },
+}
+</script>
---
+++ extensionsToTest/src/extension3/App.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+    h1 Extensão 3 (sem build)
+</template>
+
+<script>
+
+export default {
+  name: 'extension 3',
+
+}
+</script>
