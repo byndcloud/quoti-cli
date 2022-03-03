89314f5256fb5164be69dbc243dc8893e8df4e1e
Nathan Freire <nmf2@cin.ufpe.br>
2022-03-03 14:47:25 -0300

    feat: remove status from error classes

--- src/utils/erroClasses.js
+++ src/utils/erroClasses.js
@@ -1,21 +1,19 @@
 class TypedError extends Error {
-  constructor (status, type, details = '') {
-    super(`${details}`)
-    this.status = status
+  constructor (message, type) {
+    super(message)
     this.type = type
-    this.details = details
   }
 }
 
 class ExtensionNotFoundError extends TypedError {
-  constructor (msg) {
-    super('error', 'extension-not-found', msg || 'Extensão não encontrada')
+  constructor (message) {
+    super(message || 'Extensão não encontrada', 'extension-not-found')
   }
 }
 
 class ExtensionsNotFoundError extends TypedError {
-  constructor (msg) {
-    super('error', 'extensions-not-found', msg || 'Extensões não encontradas')
+  constructor (message) {
+    super(message || 'Extensões não encontradas', 'extensions-not-found')
   }
 }
 
