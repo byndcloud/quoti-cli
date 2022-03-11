f73e563cfd02fc26275b8adfc268f75f879c823e
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-03-11 17:32:13 -0300

    feat: show org on show-info-on-localhost

--- src/hooks/show-info-on-localhost.js
+++ src/hooks/show-info-on-localhost.js
@@ -1,10 +1,12 @@
 const Logger = require('../config/logger')
 const path = require('path')
 const pathEnv = path.join(__dirname, '../../.env')
+const credentials = require('../config/credentials')
 require('dotenv').config({ path: pathEnv })
 
 module.exports = async function () {
   if (process.env.API_BASE_URL?.includes('localhost')) {
-    Logger.info('**************** Você está em um ambiente localhost ****************')
+    credentials.load()
+    Logger.info(`**************** Você está em um ambiente localhost, organização: ${credentials.institution} ****************`)
   }
 }
