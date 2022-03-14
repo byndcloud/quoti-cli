849062f8ece787669f91b6e70c5bef66f9a6fe7b
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-03-14 19:11:17 -0300

    feat: test extension wihtout build

--- test/end-to-end/serve.test.js
+++ test/end-to-end/serve.test.js
@@ -6,6 +6,7 @@ const JSONManager = require('../../src/config/JSONManager')
 const path = require('path')
 const fs = require('fs')
 const VueCliService = require('@vue/cli-service')
+const Socket = require('../../src/config/socket')
 
 const testProjectRootPath = path.resolve('./extensionsToTest')
 const utilsVueCliService = require('@vue/cli-shared-utils')
@@ -29,10 +30,38 @@ describe('Serve command', () => {
   const sandbox = sinon.createSandbox()
   beforeEach(function () {
     sandbox.spy(VueCliService.prototype)
+    sandbox.spy(Socket.prototype)
   })
   afterEach(function () {
     sandbox.restore()
   })
+
+  const setupServeTestNoBuild = test
+    .add('now', Date.now())
+    .add('testProjectRootPath', testProjectRootPath)
+    .add('extensionsPaths', ctx => {
+      return utils.listExtensionsPaths(ctx.testProjectRootPath)
+    })
+    .add('manifests', ctx => {
+      return ctx.extensionsPaths.map(entryPoint => ({ entryPoint: utils.getManifestFromEntryPoint(entryPoint) }))
+    })
+    .add('distPath', ctx => path.join(ctx.testProjectRootPath, 'dist'))
+    .add('modifiedFiles', ctx => {
+      return [{
+        modifiedFilesPath: path.join(ctx.testProjectRootPath, 'src', 'extension3', 'App.vue'),
+        manifestPath: path.join(ctx.testProjectRootPath, 'src', 'extension3', 'manifest.json')
+      }]
+    })
+    .stub(credentials, 'path', beyondCredentialsPath)
+    .stub(utils, 'getProjectRootPath', () => testProjectRootPath)
+    // .stdout()
+    .command(['serve'])
+
+    .do(async ctx => {
+      await delay(1000)
+      changeFile(ctx.modifiedFiles[0].modifiedFilesPath, ctx.now)
+      await delay(1000)
+    })
 
   const setupServeTest = test
     .add('now', Date.now())
@@ -66,6 +95,12 @@ describe('Serve command', () => {
       changeFile(ctx.modifiedFiles[0].modifiedFilesPath, ctx.now)
       await delay(1000)
     })
+
+  setupServeTestNoBuild.it('Test extension without build', async (ctx, done) => {
+    const argsEmit = Socket.prototype.emit.args[0][0]
+    expect(argsEmit?.data?.code.includes(ctx.now)).to.equal(true)
+    done()
+  })
 
   setupServeTest.it('When an extension\'s file is modified vueCliService function must be called with name including dc_extensionUUID', async (ctx, done) => {
     const vueCliServiceArgs = VueCliService.prototype.run.args[0][1]
