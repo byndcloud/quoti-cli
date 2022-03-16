4e0c6c74eecfc43de8e5bfd135893a915e51025e
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-03-16 17:27:47 -0300

    feat: add projectRootPath as argument to  validateEntryPointIncludedInPackage function

--- src/utils/index.js
+++ src/utils/index.js
@@ -59,8 +59,8 @@ function listExtensionsPaths (projectRootPath) {
     path.resolve(projectRoot, extPath)
   )
 }
-function validateEntryPointIncludedInPackage (entryPointPath) {
-  const entryPointPaths = listExtensionsPaths()
+function validateEntryPointIncludedInPackage (entryPointPath, projectRootPath) {
+  const entryPointPaths = listExtensionsPaths(projectRootPath)
   if (!entryPointPaths.includes(path.resolve(entryPointPath))) {
     throw new Error(
       `O entrypoint especificado (${entryPointPath}) não está entre as extensões que já foram selecionadas. Tem certeza que o caminho está correto ou que a extensão já foi selecionada com qt select-extension?`
