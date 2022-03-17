8f118e6d381de2bcb8dc9dfaeecd57ef23f6dc3f
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-03-17 19:48:25 -0300

    feat: add getManifestSync, deleteManifestSync, setExtensionIdOnManifest and restoreManifestSync method to ExtensionClass

--- test/services/extension.js
+++ test/services/extension.js
@@ -1,8 +1,39 @@
 const path = require('path')
+const fs = require('fs')
 class Extension {
+  #manifestBufferBackup
   constructor ({ entryPoint }) {
-    this.entryPoint = path.resolve(entryPoint)
-    this.manifest = path.join(path.dirname(entryPoint), 'manifest.json')
+    this.entryPointPath = path.resolve(entryPoint)
+    this.manifestPath = path.join(path.dirname(entryPoint), 'manifest.json')
+  }
+
+  getManifestSync () {
+    return fs.readFileSync(this.manifestPath)
+  }
+
+  deleteManifestSync () {
+    this.#manifestBufferBackup = this.getManifestSync()
+    fs.unlinkSync(this.manifestPath)
+  }
+
+  /**
+   *
+   * @param {number} extensionId
+   */
+  setExtensionIdOnManifest (extensionId) {
+    const manifestBuffer = this.getManifestSync()
+    if (!this.#manifestBufferBackup) {
+      this.#manifestBufferBackup = manifestBuffer
+    }
+    const manifestObject = JSON.parse(manifestBuffer.toString())
+    manifestObject.extensionId = extensionId
+    fs.writeFileSync(this.manifestPath, JSON.stringify(manifestObject, null, 2))
+  }
+
+  restoreManifestSync () {
+    if (this.#manifestBufferBackup) {
+      fs.writeFileSync(this.manifestPath, this.#manifestBufferBackup)
+    }
   }
 }
 module.exports = Extension
