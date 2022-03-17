f06bfddcce60e24da4bc839f9b98a1bb4ece1aaa
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-03-17 19:45:02 -0300

    refactor: delete extension3NoBuild on testProject class

--- test/services/testProject.js
+++ test/services/testProject.js
@@ -5,8 +5,7 @@ class TestProject {
     this.rootPath = path.resolve('./extensionsToTest')
     this.packagePath = path.join(this.rootPath, 'package.json')
     this.extension1WithBuild = new Extension({ entryPoint: path.join(this.rootPath, 'src', 'extension1', 'App.vue') })
-    this.extension2NotFound = new Extension({ entryPoint: path.join(this.rootPath, 'src', 'extension2', 'App.vue') })
-    this.extension3NoBuild = new Extension({ entryPoint: path.join(this.rootPath, 'src', 'extension3', 'App.vue') })
+    this.extension2NoBuild = new Extension({ entryPoint: path.join(this.rootPath, 'src', 'extension2', 'App.vue') })
   }
 }
 module.exports = TestProject
