ac68e666267f03508db2b78eebc21a909e62dbe6
Nathan Freire <nmf2@cin.ufpe.br>
2022-04-06 06:13:53 -0300

    feat: the hook isn't necessary, npm validates it automatically using the engines property in the package.json

--- src/hooks/node-version.js
+++
@@ -1,9 +0,0 @@
-const semver = require('semver')
-const Logger = require('../config/logger')
-module.exports = async function (options) {
-  const nodeVersion = process.versions.node
-  if (!semver.gte(nodeVersion, '14.17.0')) {
-    Logger.error('É necessário que a versão do Node seja no mínimo 14.17.0.')
-    process.exit(0)
-  }
-}
