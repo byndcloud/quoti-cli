430ec4f75d7de52fe1ca12f7ccaf8e5639af1fad
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-05-10 18:13:07 -0300

    feat: add test to check if manifest has correct institution

---
+++ test/end-to-end/common.test.js
@@ -0,0 +1,63 @@
+const { expect, testStubLoggedIn } = require('../common/test')
+const sinon = require('sinon')
+const utils = require('../../src/utils/index')
+const ExtensionService = require('../../src/services/extension')
+
+const BaseCommand = require('../../src/base')
+const TestProject = require('../services/testProject')
+const { ManifestFromAnotherOrgError } = require('../../src/utils/errorClasses')
+const CredentialsTest = require('../services/credentials')
+
+const testProject = new TestProject()
+const credentials = new CredentialsTest()
+
+describe('Common command', function () {
+  // setup
+  const sandbox = sinon.createSandbox()
+  beforeEach(async function () {
+    sandbox.spy(ExtensionService.prototype)
+    sandbox.spy(BaseCommand.prototype)
+    await testProject.setExtensionsOnPackage([
+      testProject.extension1WithBuild,
+      testProject.extension2NoBuild
+    ])
+  })
+  afterEach(async function () {
+    await testProject.restore()
+    sandbox.restore()
+  })
+  const commonTestSetup = testStubLoggedIn.stub(
+    utils,
+    'getProjectRootPath',
+    () => testProject.rootPath
+  )
+
+  const setupCredentialsWithDifferentOrg = commonTestSetup
+    .do(() => {
+      credentials.institution = 'marketplace' // could it be any other org
+      credentials.save()
+    })
+    .command(['deploy', testProject.extension1WithBuild.entryPointPath])
+    .catch(err => {
+      expect(err).to.be.an.instanceof(ManifestFromAnotherOrgError)
+    })
+    .command(['serve', testProject.extension1WithBuild.entryPointPath])
+    .catch(err => {
+      expect(err).to.be.an.instanceof(ManifestFromAnotherOrgError)
+    })
+    .command(['publish', testProject.extension1WithBuild.entryPointPath])
+    .catch(err => {
+      expect(err).to.be.an.instanceof(ManifestFromAnotherOrgError)
+    })
+    .do(() => {
+      credentials.institution = 'beyond' // could it be any other org
+      credentials.save()
+    })
+
+  setupCredentialsWithDifferentOrg.it(
+    'qt serve/deploy/publish when manifest has no institution attribute',
+    async (_, done) => {
+      done()
+    }
+  )
+})
