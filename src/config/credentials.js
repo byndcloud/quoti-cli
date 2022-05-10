0d53fdc09ee6aa2672b97cce6a9ee756c6ebfbf9
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-05-10 17:59:19 -0300

    fix: use test credentials when NODE_ENV = test

--- src/config/credentials.js
+++ src/config/credentials.js
@@ -1,13 +1,13 @@
 const JSONManager = require('./JSONManager')
 const fs = require('fs')
 const path = require('path')
-
 const home = require('os').homedir()
 const Logger = require('../config/logger')
 this.logger = Logger.child({
   tag: 'command/publish'
 })
 const baseConfigDirectory = path.join(home, '.config/quoti-cli/')
+const CredentialsTest = require('../../test/services/credentials')
 
 try {
   fs.mkdirSync(baseConfigDirectory, { recursive: true })
@@ -18,6 +18,10 @@ try {
   this.logger.error(e)
 }
 
-module.exports = new JSONManager(
-  path.join(baseConfigDirectory, 'credentials.json')
-)
+let credentialsPath = path.join(baseConfigDirectory, 'credentials.json')
+if (process.env.NODE_ENV === 'test') {
+  const credentials = new CredentialsTest()
+  credentialsPath = credentials.credentialsPath
+}
+
+module.exports = new JSONManager(credentialsPath)
