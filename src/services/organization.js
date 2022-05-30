db09b5fb4b35538d348aa455038961f18dfaa563
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-05-30 10:52:38 -0300

    feat: add organizatition class

---
+++ src/services/organization.js
@@ -0,0 +1,27 @@
+const api = require('../config/axios')
+const { firebase } = require('../config/firebase')
+const credentials = require('../config/credentials')
+credentials.load()
+class Organization {
+  constructor ({ orgSlug } = {}) {
+    this.orgSlug = orgSlug || credentials.institution
+  }
+
+  async listDynamicComponents ({ token, orgSlug = this.orgSlug } = {}) {
+    if (!token) {
+      token = await firebase.auth().currentUser.getIdToken()
+    }
+    const address = `/${orgSlug}/dynamic-components`
+    const { data } = await api.axios.get(address, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    return data
+  }
+
+  async listExtensionsPaths ({ token } = {}) {
+    const dynamicComponents = await this.listDynamicComponents(token)
+    return dynamicComponents.map(d => d.path)
+  }
+}
+
+module.exports = Organization
