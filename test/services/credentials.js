c13ac18843f92bb8ee836cfc87747d0c68aae3f8
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-05-31 12:36:13 -0300

    fix: using env.test to tests

--- test/services/credentials.js
+++ test/services/credentials.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const fs = require('fs')
 const JSONManager = require('../../src/config/JSONManager')
+const dotenv = require('dotenv')
+dotenv.config({ path: path.join(__dirname, '../../.env.test') })
 const credentialsPath = path.resolve(process.env.TEST_BEYOND_CREDENTIALS_PATH)
 class Credentials extends JSONManager {
   constructor () {
