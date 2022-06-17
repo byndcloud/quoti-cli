dcee6cfedb9e11c2abb7b87759d0b11d5a48e750
Nathan Freire <nmf2@cin.ufpe.br>
2022-06-17 14:20:21 -0300

    build: finish publish script

--- publish.sh
+++ publish.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-# Execute npm version in the main workspace:
-  # version script will run the build
-  #   Increase package.json version in the main workspace 
-  #   and generate README.md and manifest.json
-  # postversion the changes in the main workspace
-  # Run the build script in the main workspace
-
-  # Push the changes in the main workspace
-  # Run the publish command in the CLI-deploy workspace
-  # Remove the oclif.manifest.json
-
 # Generate the manifest and readme
-npm run generate:docs 
+npm run generate:docs
+
+# Install the exact packages from package-lock
+npm ci
 
 # Copy readme and manifest to deploy workspace
 cp README.md oclif.manifest.json ./CLI-deploy 
@@ -20,14 +12,17 @@ cp README.md oclif.manifest.json ./CLI-deploy
 # Build / compact the code and save it in the deploy workspace
 npm run build 
 
-# Get the current version of the package
-VERSION=$(cat package.json | jq .version) 
+# Get the current version of the package to be published
+VERSION=$(cat package.json | jq -r .version) 
 
 # Make sure version to be published is the same as the one in the main workspace
 npm version $VERSION --workspace=CLI-deploy
 
 # Publish the code in the deploy workspace
-npm publish --workspace=CLI-deploy --dry-run
+npm publish --workspace=CLI-deploy
 
 # Remove the manifest from main and deploy workspaces
 rm -f oclif.manifest.json ./CLI-deploy/oclif.manifest.json
+
+# Add the README.md package.json from the CLI to the publish commit
+git add README.md ./CLI-deploy
