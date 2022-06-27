075bac8ec8c9c6e0511c7f4e489de9970cc18460
Luiz Albuquerque <laaj@cin.ufpe.br>
2022-06-27 13:29:09 -0300

    fix: add paramsSerializer axios on prod environment

--- src/config/axios.js
+++ src/config/axios.js
@@ -9,23 +9,22 @@ class Api {
   }
 
   async create () {
-    if (process.env.API_BASE_URL) {
-      this.axios = axios.create({
-        httpsAgent: new https.Agent({
-          rejectUnauthorized: false
-        }),
-        baseURL: process.env.API_BASE_URL,
-        // note: Essa linha permite realizar requisições para o quoti api usando apenas
-        // axios.get(url, params: {}), sem precisar passar o objeto para string
-        paramsSerializer: params => {
-          return qs.stringify(params)
-        }
-      })
-    } else {
-      this.axios = axios.create({
-        baseURL: 'https://api.minhafaculdade.app/api/v1/'
-      })
+    const config = {
+      // note: Essa linha permite realizar requisições para o quoti api usando apenas
+      // axios.get(url, params: {}), sem precisar passar o objeto para string
+      paramsSerializer: params => {
+        return qs.stringify(params)
+      }
     }
+    if (process.env.API_BASE_URL) {
+      config.httpsAgent = new https.Agent({
+        rejectUnauthorized: false
+      })
+      config.baseURL = process.env.API_BASE_URL
+    } else {
+      config.baseURL = 'https://api.minhafaculdade.app/api/v1/'
+    }
+    this.axios = axios.create(config)
   }
 }
 module.exports = new Api()
