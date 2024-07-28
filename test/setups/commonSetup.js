e6dbcb26509a2393273eebe525fdcde08baf9152
Nathan Freire <nmf2@cin.ufpe.br>
2024-07-28 17:10:55 -0300

    chore: remove unused packages and code

--- test/setups/commonSetup.js
+++ test/setups/commonSetup.js
@@ -1,26 +1,10 @@
 const { expect, test } = require('@oclif/test')
-const utilsVueCliService = require('@vue/cli-shared-utils')
-const SodaFriendlyErrorsWebpackPlugin = require('@soda/friendly-errors-webpack-plugin')
 const CredentialsTest = require('../services/credentials')
 
 const credentials = new CredentialsTest()
 credentials.createBeyondCredential()
-/**
- *
- * @param {require('@oclif/test').test} test
- */
-function suppressVueCliLogs (test) {
-  return test
-    .stub(utilsVueCliService, 'logWithSpinner', () => console.log())
-    .stub(utilsVueCliService, 'log', () => console.log())
-    .stub(utilsVueCliService, 'done', () => console.log())
-    .stub(utilsVueCliService, 'warn', () => console.log())
-    .stub(SodaFriendlyErrorsWebpackPlugin.prototype, 'displaySuccess', () =>
-      console.log()
-    )
-}
 
-const myTest = suppressVueCliLogs(test)
+const myTest = test
 module.exports = {
   testStubLoggedIn: myTest,
   expect
