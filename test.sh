6a09ec77e10ac146f13e3a14aeae74e74de0c699
Nathan Freire <nmf2@cin.ufpe.br>
2024-07-28 16:27:36 -0300

    fix: coverage breaks debugger

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 # example -> npm run teste:e2e --file=deploy
 if [ $npm_config_file ]
 then 
-    nyc mocha --forbid-only --exit "test/end-to-end/$npm_config_file.test.js"
+    mocha --forbid-only --exit "test/end-to-end/$npm_config_file.test.js"
 else
-    nyc mocha --forbid-only --exit "test/end-to-end/**/*.test.js"
+    mocha --forbid-only --exit "test/end-to-end/**/*.test.js"
 fi
