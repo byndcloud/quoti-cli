cb302867054009fa264db06321b2c9196d30af3f
Nathan Freire <nmf2@cin.ufpe.br>
2024-07-28 17:08:23 -0300

    Publish 1.0.0-beta

--- publish.sh
+++ publish.sh
@@ -15,8 +15,9 @@ npm run build
 # Get the current version of the package to be published
 VERSION=$(cat package.json | jq -r .version) 
 
-# Get a possible publish tag from the package version
-PUBLISH_TAG=$(echo $VERSION | grep -oP '(?<=-)(\w+)(?=\.)?')
+# Get a possible publish tag from command line, if not set, it will publish to
+# "latest"
+PUBLISH_TAG=$1
 
 # Make sure version to be published is the same as the one in the main workspace
 npm version $VERSION --workspace=CLI-deploy
@@ -24,11 +25,11 @@ npm version $VERSION --workspace=CLI-deploy
 if [ -z $PUBLISH_TAG  ]; then
   # Publish the code to NPM with the "latest" tag
   echo "Publish to latest tag $PUBLISH_TAG"
-  npm publish --workspace=CLI-deploy --tag latest 
+  echo "npm publish --workspace=CLI-deploy"
 else
   # Publish the code to NPM with the tag from the package version
   echo "Publish to specific tag: $PUBLISH_TAG"
-  npm publish --workspace=CLI-deploy --tag $PUBLISH_TAG
+  echo "npm publish --workspace=CLI-deploy --tag $PUBLISH_TAG"
 fi
 
 # Remove the manifest from main and deploy workspaces
